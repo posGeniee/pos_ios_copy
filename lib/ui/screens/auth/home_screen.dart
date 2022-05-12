0c507529462121420aaf2f164798ac06bebf9ddc
shahryar-cmyk <posgeniee@gmail.com>
2022-05-12 08:14:27 +0500

    cupertino removed.

--- lib/ui/screens/auth/home_screen.dart
+++ lib/ui/screens/auth/home_screen.dart
@@ -1,10 +1,7 @@
 import 'package:dummy_app/data/network/auth_request.dart';
-import 'package:dummy_app/main.dart';
-import 'package:dummy_app/services/network_service.dart';
 import 'package:dummy_app/ui/screens/after_sign_in.dart';
 import 'package:dummy_app/ui/screens/auth/sign_in_screen.dart';
 // import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart ';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
